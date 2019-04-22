871b4c6db51b075955697a2e9673489af5cd8016
joao7yt <joao7yt@gmail.com>
2019-04-22 15:41:18 -0300

    messing with and trying to implementing selctor, that will be used for selecting the desired bridge and the lights for each event

---
+++ CSHUE/Views/Selector.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CSHUE.Views
+{
+    /// <summary>
+    /// Interaction logic for Selector.xaml
+    /// </summary>
+    public partial class Selector : Window
+    {
+        public string Ok
+        {
+            get => (string)GetValue(OkProperty);
+            set => SetValue(OkProperty, value);
+        }
+        public static readonly DependencyProperty OkProperty =
+            DependencyProperty.Register("Ok", typeof(string), typeof(Selector));
+
+        public Selector()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonOk_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            Close();
+        }
+
+        public List<BridgeSelector> List
+        {
+            get => (List<BridgeSelector>)GetValue(ListProperty);
+            set => SetValue(ListProperty, value);
+        }
+        public static readonly DependencyProperty ListProperty =
+            DependencyProperty.Register("List", typeof(List<BridgeSelector>), typeof(Selector));
+    }
+}

dc5bd0cc50e99699b5feb0a3555e2959bd18fce5
joao7yt <joao7yt@gmail.com>
2019-05-13 16:02:08 -0300

    code cleanup

--- CSHUE/Controls/LoadingSpinner.xaml.cs
+++ CSHUE/Controls/LoadingSpinner.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Windows;
-using System.Windows.Shapes;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 // ReSharper disable CompareOfFloatsByEqualityOperator

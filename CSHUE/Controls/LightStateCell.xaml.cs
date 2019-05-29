18a1acded4c6bcd8848cfacddffa2cd64750d364
joao7yt <joao7yt@gmail.com>
2019-05-29 11:06:15 -0300

    code cleanup

--- CSHUE/Controls/LightStateCell.xaml.cs
+++ CSHUE/Controls/LightStateCell.xaml.cs
@@ -3,7 +3,6 @@ using System.Threading;
 using System.Windows;
 using System.Windows.Media;
 // ReSharper disable AccessToModifiedClosure
-
 // ReSharper disable InheritdocConsiderUsage
 
 namespace CSHUE.Controls
--- CSHUE/ViewModels/AboutViewModel.cs
+++ CSHUE/ViewModels/AboutViewModel.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Reflection;
-
-namespace CSHUE.ViewModels
+﻿namespace CSHUE.ViewModels
 {
     public class AboutViewModel : BaseViewModel
     {

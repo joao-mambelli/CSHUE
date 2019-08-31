7bec178224665973552c272135dcfc4276f60a70
joao7yt <joao7yt@gmail.com>
2019-08-31 02:38:40 -0300

    added update page

---
+++ CSHUE/ViewModels/UpdateViewModel.cs
@@ -0,0 +1,29 @@
+﻿namespace CSHUE.ViewModels
+{
+    public class UpdateViewModel : BaseViewModel
+    {
+        #region Properties
+
+        private string _link;
+        public string Link
+        {
+            get => _link;
+            set
+            {
+                _link = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region Fields
+
+        /// <summary>
+        /// Main window viewmodel field.
+        /// </summary>
+        public MainWindowViewModel MainWindowViewModel = null;
+
+        #endregion
+    }
+}
---
+++ CSHUE/Views/Update.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows;
+using CSHUE.ViewModels;
+
+namespace CSHUE.Views
+{
+    /// <summary>
+    /// Interaction logic for Update.xaml
+    /// </summary>
+    public partial class Update
+    {
+        #region Fields
+
+        /// <summary>
+        /// ViewModel field.
+        /// </summary>
+        public UpdateViewModel ViewModel = new UpdateViewModel();
+
+        #endregion
+
+        #region Initializers
+
+        /// <summary>
+        /// Update initializer.
+        /// </summary>
+        public Update()
+        {
+            InitializeComponent();
+            DataContext = ViewModel;
+
+            ViewModel.MainWindowViewModel = Application.Current.Windows.OfType<MainWindow>().FirstOrDefault()?.ViewModel;
+
+            ViewModel.Link = $"https://github.com/joao7yt/CSHUE/releases/tag/{Properties.Settings.Default.LatestVersionCheck}";
+        }
+
+        #endregion
+
+        #region Events Handlers
+
+        /// <summary>
+        /// ShowInBrowser button click event handler.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ShowInBrowser_OnClick(object sender, EventArgs e)
+        {
+            Process.Start($"https://github.com/joao7yt/CSHUE/releases/tag/{Properties.Settings.Default.LatestVersionCheck}");
+        }
+
+        #endregion
+    }
+}

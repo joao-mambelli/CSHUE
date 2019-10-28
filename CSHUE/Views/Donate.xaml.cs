35c3c5356f12a6e1c583805e320915c3124a6a1d
Joao YT <joao7yt@gmail.com>
2019-10-28 14:02:03 -0300

    added german language and reorganized tabs

--- CSHUE/Views/Donate.xaml.cs
+++ CSHUE/Views/Donate.xaml.cs
@@ -45,21 +45,10 @@ namespace CSHUE.Views
         private void Donate_OnClick(object sender, EventArgs e)
         {
             var url = "";
-
-            const string business = "joao7yt@gmail.com";
-            var description = Cultures.Resources.DonationDescription;
-            var country = Cultures.Resources.DonationCountry;
-            var currency = Cultures.Resources.DonationCurrency;
-
-            url += "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=" +
-                   business +
-                   "&lc=" +
-                   country +
-                   "&item_name=" +
-                   description +
-                   "&currency_code=" +
-                   currency +
-                   "&bn=PP%2dDonationsBF";
+            
+            url += "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=joao7yt@gmail.com&lc=" +
+                   Cultures.Resources.CountryIso2 +
+                   "&item_name=CSHUE&currency_code=USD&bn=PP%2dDonationsBF";
 
             Process.Start(url);
         }
